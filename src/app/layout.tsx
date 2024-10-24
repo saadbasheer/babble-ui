e7e8a2f2f5357a1ee30307157c54e509d3aa9888
saadbasheer <saadbasheer0@gmail.com>
2024-10-24 18:59:37 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Particles from "@/components/ui/Particles";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <Particles className="absolute z-3 w-full h-full  bg-[#2F4858]" />
         {children}
+        <Analytics />
       </body>
     </html>
   );
